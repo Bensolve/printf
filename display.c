dc9277b6cfcfa20d87cd7c262add43f3e00c97ac
bensolve <benkissa10@gmail.com>
2023-08-24 16:15:17 -0700

    try

--- display.c
+++ display.c
@@ -16,19 +16,19 @@ int _printf(const char *format, ...)
 	for (i = 0; i < nums; i++)
 	{
 		if (format[i] != '%')
-			write(1, format[i], 1);
+			write(1, &format[i], 1);
 		else 
 		{
 			i++;
-			if (format[i] == 's');
+			if (format[i] == 's')
 			{
 				char *chars = va_arg(texts, char *);
 				write(1, chars, 1);
 			}
-			else if (format[i] == 'c');
+			else if (format[i] == 'c')
 			{
 				char text = va_arg(texts, int);
-				write(1, text, 1);
+				write(1, &text, 1);
 			}
 		}
 	}
