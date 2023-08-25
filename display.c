e1a17db8cf0107e874fcfe8da0b3a48b55e03fd4
bensolve <benkissa10@gmail.com>
2023-08-25 06:50:02 -0700

    nn

--- display.c
+++ display.c
@@ -9,31 +9,59 @@
 int _printf(const char *format, ...)
 {
 	int i;
-	int nums = strlen(format);
+	int nums = 0;
 	va_list texts;
+
+	if (format == NULL)
+		return (-1);
 
 	va_start(texts, format);
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
+		{
 			write(1, &format[i], 1);
-		else 
+			nums++;
+		}
+		else
 		{
 			i++;
+			if (format[i] == '\0')
+				break;
+
+			if (format[i] == '%')
+			{
+				write(1, &format, 1);
+				nums++;
+			}
 			if (format[i] == 's')
 			{
 				char *chars = va_arg(texts, char *);
-				while (*chars != '\0')
+				int lens = 0;
+
+				while (*(chars + lens) != '\0')
 				{
-					write(1, chars, 1);
-					chars++;
+					lens++;
 				}
+				write(1, chars, lens);
+				nums += lens;
 			}
 			else if (format[i] == 'c')
 			{
 				char text = va_arg(texts, int);
+
 				write(1, &text, 1);
+				nums++;
 			}
+			else if (format[i] == 'd')
+			{
+				int x = va_arg(texts, int);
+
+				write(1, &x, 1);
+				nums++;
+			}
+
+
 		}
 	}
 	va_end(texts);
